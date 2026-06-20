4dd5a0164ad2422b0512d61632d4a0d3dbfd84e0
Le Quan <lequan13112005@gmail.com>
2026-06-20 09:34:35 +0700

    first commit: create packages

---
+++ src/main/java/com/quan/apartment_building_management_system/ApartmentBuildingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.quan.apartment_building_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentBuildingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartmentBuildingManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quan/apartment_building_management_system/ApartmentBuildingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quan.apartment_building_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentBuildingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
