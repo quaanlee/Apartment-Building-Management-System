19ba3c6baf906d70c5c05df1ef2cc4e7af774c20
loitaisonFPT <yeucaurat@gmail.com>
2026-06-29 23:37:18 +0700

    Done view detail profile(resident:apartment, householdMember, Vehicle, Bill, ... || maintenance: task, ...)

--- src/main/java/com/quan/apartment_building_management_system/repository/ProfileRepository.java
+++ src/main/java/com/quan/apartment_building_management_system/repository/ProfileRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,6 +18,8 @@ public interface ProfileRepository extends JpaRepository<Profile, Integer> {
     Optional<Profile> findByAccountAccountId(Integer accountId);
 
     Optional<Profile> findByCitizenId(String citizenId);
+
+    List<Profile> findByApartmentApartmentId(Integer apartmentId);
 
     @Query("SELECT p FROM Profile p LEFT JOIN FETCH p.account a LEFT JOIN FETCH a.role LEFT JOIN FETCH p.apartment WHERE " +
            "(:search IS NULL OR :search = '' OR LOWER(p.fullName) LIKE LOWER(CONCAT('%', :search, '%')) OR LOWER(p.citizenId) LIKE LOWER(CONCAT('%', :search, '%')) OR LOWER(p.email) LIKE LOWER(CONCAT('%', :search, '%'))) AND " +
